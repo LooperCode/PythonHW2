6f97b180392f420ca6a76995d2fa58c0bbad0702
LooperCode <savchenkodkz@gmail.com>
2023-02-03 02:34:55 +0500

    fib

---
+++ fib.py
@@ -0,0 +1,17 @@
+def fib(index):
+    if index == 1:
+        return 0
+    elif index == 2:
+        return 1
+    return fib(index-1) + fib(index-2)
+
+
+index = int(input("Введите количество чисел Фибоначчи:\n"))
+list = [fib(index) for index in range(1, index+2)]
+print(list)
+list = list[::-1] + list[1:]
+for i in range(len(list)):
+    if i%2 == 0 and i < len(list)//2:
+        list[i] = -list[i]
+print(list, '\n')
+        
